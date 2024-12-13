4da0f4f2247d8a668d932b3bb1771260d8b6a408
Ivan Caviedes <ivancaviedes99@gmail.com>
2024-12-12 19:45:22 -0500

    mensaje santiago

--- src/data/jugadores.ts
+++ src/data/jugadores.ts
@@ -1,23 +1,32 @@
 export default [
   {
-    "name": "Alejandra",
-    "message": "¡Hola, querido amigo secreto! 🎅✨ Quiero contarte que me encantan los pines divertidos asi como los de dibujos animados, y me gusta el chocolate blanco 🍫🤍. Creo que podrían ser una súper idea de regalo, osea weon 🎁. Estoy emocionada por esta experiencia y seguro que lo que elijas será genial. si no lo boto ¡Gracias por la magia navideña! jo-jo-jo feli navida y propero año nevo🎄🎉"
-  },
-  { "name": "Santiago", "message": "No quiero nada" },
-  {
-    "name": "Luisa",
-    "message": "Querido amigo secreto! 🎅🎁 Quiero contarte que me gusta mucho Harry Potter. Así que, si buscas ideas, cualquier cosa relacionada con eso me hará muy feliz."
+    name: "Alejandra",
+    message:
+      "¡Hola, querido amigo secreto! 🎅✨ Quiero contarte que me encantan los pines divertidos asi como los de dibujos animados, y me gusta el chocolate blanco 🍫🤍. Creo que podrían ser una súper idea de regalo, osea weon 🎁. Estoy emocionada por esta experiencia y seguro que lo que elijas será genial. si no lo boto ¡Gracias por la magia navideña! jo-jo-jo feli navida y propero año nevo🎄🎉",
   },
   {
-    "name": "Sebastian",
-    "message": "Hola, querido amigo secreto: Soy una persona que disfruta las cosas que nos hacen sentir bien: un buen libro que te atrape, ropa cómoda que te haga ver y sentir genial, y, por supuesto, algo rico para disfrutar al paladar. Este año tengo un pequeño detalle: Estoy con un pie que necesita descanso y paciencia, así que cualquier regalo que me ayude a pasar el rato será más que bienvenido. 😉 ¡Confío en que tu creatividad dará en el clavo! 🎁"
+    name: "Santiago",
+    message:
+      "Querido Santa Claus, Espero que te encuentres muy bien junto a tus renos y que estés listo para repartir alegría este año. He sido muy bueno y responsable este año, y por eso me gustaría pedirte algo especial para esta Navidad. Me encantaría recibir un saco oversize. Esos suéteres grandes y cómodos que son perfectos para sentirme abrigado y relajado en cualquier momento. Me gusta mucho usar ropa de moda y estoy seguro de que será el regalo ideal para mí. No importa el color , pero si puedes elegir uno que combine con todo, sería genial. Confío en tu buen gusto. Gracias por todo el esfuerzo que haces cada año para cumplir los deseos de todos. Prometo seguir siendo amable con los demas y ayudar en lo que pueda. ¡Te mando un abrazo grande y espero que tengas un viaje seguro!Con mucho cariño, Santiago",
   },
   {
-    "name": "Andres",
-    "message": "Querido santa he sido un niño 💕 bueno todo el año por eso quiero pedirte de navidad algo cool que tenga que ver con el espacio o las estrellas o harry Potter bien cool la buena pal que le salga 😎😉💖🫠"
+    name: "Luisa",
+    message:
+      "Querido amigo secreto! 🎅🎁 Quiero contarte que me gusta mucho Harry Potter. Así que, si buscas ideas, cualquier cosa relacionada con eso me hará muy feliz.",
   },
   {
-    "name": "Ivan",
-    "message": "Hola, querido amigo secreto: Soy alguien que disfruta de los videojuegos y la tecnología. También me encanta pasar tiempo viendo películas y compartiendo momentos con amigos o familia. Me gustan los detalles sencillos y prácticos, y realmente aprecio cualquier cosa que venga con cariño. No hay nada que quiera evitar, así que confío en tu creatividad para sorprenderme. ¡Gracias desde ya por el regalo! 😊"
-  }
-]
+    name: "Sebastian",
+    message:
+      "Hola, querido amigo secreto: Soy una persona que disfruta las cosas que nos hacen sentir bien: un buen libro que te atrape, ropa cómoda que te haga ver y sentir genial, y, por supuesto, algo rico para disfrutar al paladar. Este año tengo un pequeño detalle: Estoy con un pie que necesita descanso y paciencia, así que cualquier regalo que me ayude a pasar el rato será más que bienvenido. 😉 ¡Confío en que tu creatividad dará en el clavo! 🎁",
+  },
+  {
+    name: "Andres",
+    message:
+      "Querido santa he sido un niño 💕 bueno todo el año por eso quiero pedirte de navidad algo cool que tenga que ver con el espacio o las estrellas o harry Potter bien cool la buena pal que le salga 😎😉💖🫠",
+  },
+  {
+    name: "Ivan",
+    message:
+      "Hola, querido amigo secreto: Soy alguien que disfruta de los videojuegos y la tecnología. También me encanta pasar tiempo viendo películas y compartiendo momentos con amigos o familia. Me gustan los detalles sencillos y prácticos, y realmente aprecio cualquier cosa que venga con cariño. No hay nada que quiera evitar, así que confío en tu creatividad para sorprenderme. ¡Gracias desde ya por el regalo! 😊",
+  },
+];
